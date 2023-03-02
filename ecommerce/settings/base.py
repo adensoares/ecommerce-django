838eedef951e41ca348b00be60a090cc01437145
Aden Soares <adensoares@gmail.com>
2023-03-02 20:49:28 -0300

    Added whitenoise in the  MIDDLEWARE

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = DJANGO_CORE_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
