327faaadb83ffb55aeb9486966b2f4141b1ba39a
Aden Soares <adensoares@gmail.com>
2023-03-03 09:38:44 -0300

    Changed back the index and urls of index

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,15 +17,15 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from api.views import index
-# from django.views.generic import TemplateView
+# from api.views import index
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', index, name="index")
-    # re_path('.*', TemplateView.as_view(template_name='index.html'))
+    # path('', index, name="index")
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 
 ]
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
